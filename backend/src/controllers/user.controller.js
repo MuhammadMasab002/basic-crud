c57b9a41c746b0d02fc5ef2f2b99047dc8820691
MuhammadMasab002 <muhammadmasab2002@gmail.com>
2025-12-26 18:05:40 +0500

    implement getUser API (Home, table)

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -15,7 +15,9 @@ const createUser = async (req, res) => {
       return res.status(500).json({ message: "Failed to create user" });
     }
 
-    return res.status(201).json({ message: "User created successfully" });
+    return res
+      .status(201)
+      .json({ message: "User created successfully", data: newUser });
   } catch (error) {
     return res.status(500).json({ message: "Internal Server Error" });
   }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import Form from "../components/Form";
 import Table from "../components/Table";
+import { useGetUserQuery } from "../services/api";
 
 function Home() {
+  // implement crud api for user endpoints using base query from redux toolkit query
+  const {
+    data: userData,
+    error: isError,
+    isLoading: isLoadingUser,
+  } = useGetUserQuery();
+
+  isError && console.log("Error fetching users:", isError);
+
   return (
     <div className="w-full min-h-screen flex flex-col justify-center items-center gap-4 bg-gray-50">
       <Form />
-      <Table />
+      <Table userData={userData?.data} isLoading={isLoadingUser} />
     </div>
   );
 }
