1ef0a5c2b85cb69d0bca328547f678cc8cccb4a1
MuhammadMasab002 <muhammadmasab2002@gmail.com>
2025-12-30 17:53:09 +0500

    fix: update API base URL to production

--- frontend/src/services/api/index.js
+++ frontend/src/services/api/index.js
@@ -4,7 +4,7 @@ import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://basic-crud-backend-sandy.vercel.app/api",
   }),
   tagTypes: ["USER"],
   endpoints: (builder) => ({
